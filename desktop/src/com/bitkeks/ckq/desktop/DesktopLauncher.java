04e388e25198a5a1a61808caae2f9f7a43a5b93a
complover116 <complover116@gmail.com>
2015-12-05 13:07:00 +0300

    Changed KumQuat to Burning Maze

--- desktop/src/com/bitkeks/ckq/desktop/DesktopLauncher.java
+++ desktop/src/com/bitkeks/ckq/desktop/DesktopLauncher.java
@@ -7,6 +7,7 @@ import com.bitkeks.ckq.KumQuat;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Burning Maze";
 		new LwjglApplication(new KumQuat(), config);
 	}
 }
